63ecd0edabf0d45ba057a7d6d3e5b968c83c4456
zerefwayne <aayushjog@gmail.com>
2020-12-07 21:03:50 +0530

    [polytope] Replace poly with lines

--- src/polytypes.jl
+++ src/polytypes.jl
@@ -1,17 +1,34 @@
-function default_theme(scene::SceneLike, ::Type{<: Plot(Polytope)})
+function default_theme(scene::SceneLike, ::Type{<:Plot(Polytope)})
     Theme(
-        color = :red,
-        strokewidth = 3
+        color = :black,
+        strokewidth = 2
     )
 end
+
+using Meshes
 
 """
     Function to plot all types which are type Meshes.Polytype
 """
 function AbstractPlotting.plot!(plot::Plot(Polytope))
     pt = to_value(plot[1])
+    # convert StaticArray to Array
     v = coordinates.(vertices(pt))
-    poly!(plot, v, color = :transparent, strokecolor = plot[:color], strokewidth = plot[:strokewidth])
     scatter!(plot, v, color = plot[:color])
+    # push first element to create complete the polygon
+    # [(1, 0), (0, 0), (0, 1)] -> [(1, 0), (0, 0), (0, 1), (1, 0)]
+    v = Array.(v)
+    push!(v, v[1])
+    # extract x indices
+    xs = ((x) -> x[1]).(v)
+    # extract y indices
+    ys = ((x) -> x[2]).(v)
+    if embeddim(pt) == 2
+        lines!(plot, xs, ys)
+    elseif embeddim(pt) == 3
+        # if third dimension exists, extract z indices
+        zs = ((x) -> x[3]).(v)
+        lines!(plot, xs, ys, zs)
+    end
     plot
 end
