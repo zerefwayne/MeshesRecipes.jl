d515c708f7f3fb357f0c864aad9e524ebfd464d8
zerefwayne <aayushjog@gmail.com>
2020-12-15 19:59:44 +0530

    [polytope] Tab sizes

--- src/polytypes.jl
+++ src/polytypes.jl
@@ -1,6 +1,6 @@
 function default_theme(scene::SceneLike, ::Type{<:Plot(Polytope)})
-	Theme(
-		color=:black,
+  Theme(
+    color=:black,
     strokewidth=2
   )
 end
@@ -8,17 +8,17 @@ end
 import AbstractPlotting: Attributes
 
 """
-	Function to plot general Polytope objects.
+  Function to plot general Polytope objects.
 """
 function AbstractPlotting.plot!(plot::Plot(Polytope))
-	pt = to_value(plot[1])
-	v = coordinates.(vertices(pt))
-	scatter!(plot, v, color=plot[:color])
-	edges = facets(pt)
-	for edge in edges
-			v = edge.vertices
-			vc = coordinates.(v)
-			lines!(plot, vc)
-	end
-	plot
+  pt = to_value(plot[1])
+  v = coordinates.(vertices(pt))
+  scatter!(plot, v, color=plot[:color])
+  edges = facets(pt)
+  for edge in edges
+      v = edge.vertices
+      vc = coordinates.(v)
+      lines!(plot, vc)
+  end
+  plot
 end
