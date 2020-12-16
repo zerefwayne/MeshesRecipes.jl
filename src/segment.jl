b8ce16aa7b47927bfc7e8a1717f89ebbc302733d
zerefwayne <aayushjog@gmail.com>
2020-12-16 19:31:46 +0530

    [segment] Add plot function for Segment

---
+++ src/segment.jl
@@ -0,0 +1,14 @@
+import AbstractPlotting
+using Meshes
+
+AbstractPlotting.plot(s::Segment; kwargs...) = plot!(AbstractPlotting.Scene(), s; kwargs...)
+AbstractPlotting.plot!(s::Segment; kwargs...) = plot!(AbstractPlotting.current_scene(), s; kwargs...)
+AbstractPlotting.plot!(scene::AbstractPlotting.Scene, s::Segment; kwargs...) = plot!(scene, s; kwargs...)
+
+function AbstractPlotting.plot!(scene::AbstractPlotting.Scene, s::Segment; kwargs...)
+  v = coordinates.(vertices(s))
+  lines!(scene, v, kwargs...)
+  scatter!(scene, v, kwargs...)
+end
+
+
