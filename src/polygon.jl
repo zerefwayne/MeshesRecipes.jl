73f0c3ccb8bc287974e789fbce1480dc3e30381e
zerefwayne <aayushjog@gmail.com>
2020-12-16 20:53:16 +0530

    [Polygon] Add transparent fill kw

--- src/polygon.jl
+++ src/polygon.jl
@@ -2,9 +2,13 @@ AbstractPlotting.plot(p::Polygon; kwargs...) = plot!(AbstractPlotting.Scene(), p
 AbstractPlotting.plot!(p::Polygon; kwargs...) = plot!(AbstractPlotting.current_scene(), p; kwargs...)
 AbstractPlotting.plot!(scene::AbstractPlotting.Scene, p::Polygon; kwargs...) = plot!(scene, p; kwargs...)
 
-function AbstractPlotting.plot!(scene::AbstractPlotting.Scene, p::Polygon; kwargs...)
+import AbstractPlotting: RGBAf0
+
+function AbstractPlotting.plot!(scene::AbstractPlotting.Scene, p::Polygon; fill=true, kwargs...)
   for facet in facets(p)
     plot!(scene, facet)
   end
+  pts = coordinates.(vertices(p))
+  fill && mesh!(pts, color=RGBAf0(0, 0, 0, 0.6))
   scene
 end
